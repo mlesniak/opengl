03a23c8026da7e0e5c3f09194d75efe00646bf23
Michael Lesniak <mail@mlesniak.com>
2020-11-10 21:02:08 +0100

    Add colorization shader

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	// and looks at the origin
 	// Head is up (set to 0,-1,0 to look upside-down)
 	view := mgl32.LookAt(
-		4, 3, 3,
+		4, 3, -3,
 		0, 0, 0,
 		0, 1, 0,
 	)
@@ -86,10 +86,54 @@ func main() {
 		1.0, -1.0, 1.0,
 	}
 
+	colors := []float32{
+		0.583, 0.771, 0.014,
+		0.609, 0.115, 0.436,
+		0.327, 0.483, 0.844,
+		0.822, 0.569, 0.201,
+		0.435, 0.602, 0.223,
+		0.310, 0.747, 0.185,
+		0.597, 0.770, 0.761,
+		0.559, 0.436, 0.730,
+		0.359, 0.583, 0.152,
+		0.483, 0.596, 0.789,
+		0.559, 0.861, 0.639,
+		0.195, 0.548, 0.859,
+		0.014, 0.184, 0.576,
+		0.771, 0.328, 0.970,
+		0.406, 0.615, 0.116,
+		0.676, 0.977, 0.133,
+		0.971, 0.572, 0.833,
+		0.140, 0.616, 0.489,
+		0.997, 0.513, 0.064,
+		0.945, 0.719, 0.592,
+		0.543, 0.021, 0.978,
+		0.279, 0.317, 0.505,
+		0.167, 0.620, 0.077,
+		0.347, 0.857, 0.137,
+		0.055, 0.953, 0.042,
+		0.714, 0.505, 0.345,
+		0.783, 0.290, 0.734,
+		0.722, 0.645, 0.174,
+		0.302, 0.455, 0.848,
+		0.225, 0.587, 0.040,
+		0.517, 0.713, 0.338,
+		0.053, 0.959, 0.120,
+		0.393, 0.621, 0.362,
+		0.673, 0.211, 0.457,
+		0.820, 0.883, 0.371,
+		0.982, 0.099, 0.879,
+	}
+
 	var vertextbuffer uint32
 	gl.GenBuffers(1, &vertextbuffer)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertextbuffer)
 	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var colorbuffer uint32
+	gl.GenBuffers(1, &colorbuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, colorbuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(colors), gl.Ptr(colors), gl.STATIC_DRAW)
 
 	gl.ClearColor(0, 0, 0.4, 1)
 	for !window.ShouldClose() {
@@ -110,6 +154,16 @@ func main() {
 		gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
 		gl.DrawArrays(gl.TRIANGLES, 0, 12*3)
 		gl.DisableVertexAttribArray(0)
+
+		gl.EnableVertexAttribArray(1)
+		gl.BindBuffer(gl.ARRAY_BUFFER, colorbuffer)
+		gl.VertexAttribPointer(
+			1, // layout(location = 1)
+			3,
+			gl.FLOAT,
+			false,
+			0,
+			nil)
 
 		window.SwapBuffers()
 		glfw.PollEvents()
