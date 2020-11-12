77c2112cac0dd9ba70cf224d613b23e1b74b498d
Michael Lesniak <mail@mlesniak.com>
2020-11-12 21:03:10 +0100

    Create shaders

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	_ "image/png"
 	"log"
 	"runtime"
+	"strings"
 	//"github.com/go-gl/mathgl/mgl32"
 )
 
@@ -24,6 +25,63 @@ func main() {
 }
 
 func render(window *glfw.Window) {
+	vertices := []float32{
+		-0.5, -0.5, 0.0,
+		0.5, -0.5, 0.0,
+		0.0, 0.5, 0.0,
+	}
+
+	// Send vertices to GPU memory for later consumption.
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(vertices), gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	// Compile shaders.
+	var vertexShader uint32
+	vertexShader = gl.CreateShader(gl.VERTEX_SHADER)
+	csources, free := gl.Strs(vertexShaderSource)
+	gl.ShaderSource(vertexShader, 1, csources, nil)
+	free()
+	gl.CompileShader(vertexShader)
+
+	var status int32
+	gl.GetShaderiv(vertexShader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(vertexShader, gl.INFO_LOG_LENGTH, &logLength)
+		logs := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(vertexShader, logLength, nil, gl.Str(logs))
+		log.Printf("failed to compile %v: %v", vertexShaderSource, logs)
+	}
+
+	var fragmentShader uint32
+	fragmentShader = gl.CreateShader(gl.FRAGMENT_SHADER)
+	csources, free = gl.Strs(fragmentShaderSource)
+	gl.ShaderSource(fragmentShader, 1, csources, nil)
+	free()
+	gl.CompileShader(fragmentShader)
+
+	gl.GetShaderiv(fragmentShader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(fragmentShader, gl.INFO_LOG_LENGTH, &logLength)
+		logs := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(fragmentShader, logLength, nil, gl.Str(logs))
+		log.Printf("failed to compile %v: %v", fragmentShaderSource, logs)
+	}
+
+	// Create program combining shaders.
+	var shaderProgram uint32
+	shaderProgram = gl.CreateProgram()
+	gl.AttachShader(shaderProgram, vertexShader)
+	gl.AttachShader(shaderProgram, fragmentShader)
+	gl.LinkProgram(shaderProgram)
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+	// Every shader and rendering call after glUseProgram will now use this program object (and thus the shaders).
+	gl.UseProgram(shaderProgram)
+
 	for !window.ShouldClose() {
 		processInput(window)
 
@@ -68,3 +126,23 @@ func initialize() *glfw.Window {
 
 	return window
 }
+
+var vertexShaderSource = `
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+` + "\x00"
+
+var fragmentShaderSource = `
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+} 
+` + "\x00"
